cdedcb90b52032a1afee0a0c0f69d2a52f02e495
Anshuman <anshuman789905@gmail.com>
2026-08-04 20:10:33 +0530

    Trapping Rainwater Problem

---
+++ Array/TwoPointers/Dsa-02/TrappingrainWater.java
@@ -0,0 +1,51 @@
+// Two Pointer Pattern (Trapping Rain Water)
+
+public class TrappingrainWater {
+
+    public static int trap(int[] height) {
+
+        // Initialize pointers
+        int left = 0;
+        int right = height.length - 1;
+
+        // Initialize variables
+        int leftMax = 0;
+        int rightMax = 0;
+        int water = 0;
+
+        while (left < right) {
+
+            if (height[left] < height[right]) {
+
+                if (height[left] >= leftMax) {
+                    leftMax = height[left];
+                } else {
+                    water += leftMax - height[left];
+                }
+
+                left++;
+
+            } else {
+
+                if (height[right] >= rightMax) {
+                    rightMax = height[right];
+                } else {
+                    water += rightMax - height[right];
+                }
+
+                right--;
+            }
+        }
+
+        return water;
+    }
+
+    public static void main(String[] args) {
+
+        int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        int result = trap(height);
+
+        System.out.println(result);
+    }
+}
