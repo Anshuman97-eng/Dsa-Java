7dec39aff915ed1c58386a515da113cc4a4624cb
Anshuman <anshuman789905@gmail.com>
2026-08-07 19:13:49 +0530

    SubarraySumEqualsK problem

--- Array/HashMap/Dsa-02/SubarraySumEqualsK.java
+++ Array/HashMap/Dsa-02/SubarraySumEqualsK.java
@@ -1,5 +1,4 @@
 import java.util.HashMap;
-import java.util.HashSet;
 
 /*
  * Problem Name : Subarray Sum Equals K (LeetCode 560)
@@ -24,7 +23,7 @@ import java.util.HashSet;
  * Old Prefix Sum = Current Prefix Sum - k
  *
  * So we check:
- * map.containsKey(prefixSum - k)
+ * map.containsKey(prefix - k)
  *
  * Time Complexity  : O(n)
  * Space Complexity : O(n)
@@ -34,22 +33,29 @@ public class SubarraySumEqualsK {
 
     public static int subarraySum(int[] nums, int k) {
 
-        int prefix =0 ;
-        HashMap<Integer,Integer> map = new HashMap<>();
+        int prefix = 0;
+        int count = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        // Store prefix sum 0 initially
+        // It helps to find subarrays starting from index 0
         map.put(0, 1);
 
-        int count =0;
+        for (int num : nums) {
 
-        for(int num : nums){
+            // Calculate current prefix sum
             prefix += num;
 
-            if(map.containsKey(prefix-k)){
-                count += map.get(prefix-k);
+            // Check if required old prefix exists
+            if (map.containsKey(prefix - k)) {
+
+                count += map.get(prefix - k);
             }
 
-            map.put(prefix, map.getOrDefault(map.get(prefix - k), 0)+1);
+            // Store current prefix sum frequency
+            map.put(prefix, map.getOrDefault(prefix, 0) + 1);
         }
-
 
         return count;
     }
