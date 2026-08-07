0e6c47a8e15c28b3e9ba60a46fedbc9be8e258fc
Anshuman <anshuman789905@gmail.com>
2026-08-07 19:02:06 +0530

    SubarraySumEqualsK problem

---
+++ Array/HashMap/Dsa-02/SubarraySumEqualsK.java
@@ -0,0 +1,66 @@
+import java.util.HashMap;
+import java.util.HashSet;
+
+/*
+ * Problem Name : Subarray Sum Equals K (LeetCode 560)
+ *
+ * Pattern Used : Prefix Sum + HashMap
+ *
+ * Input  : int[] nums, int k
+ * Output : int
+ *
+ * Definition:
+ * Count the number of continuous subarrays
+ * whose sum is exactly equal to k.
+ *
+ * Hint:
+ *
+ * Subarray Sum = Current Prefix Sum - Old Prefix Sum
+ *
+ * We need:
+ * Current Prefix Sum - Old Prefix Sum = k
+ *
+ * Therefore:
+ * Old Prefix Sum = Current Prefix Sum - k
+ *
+ * So we check:
+ * map.containsKey(prefixSum - k)
+ *
+ * Time Complexity  : O(n)
+ * Space Complexity : O(n)
+ */
+
+public class SubarraySumEqualsK {
+
+    public static int subarraySum(int[] nums, int k) {
+
+        int prefix =0 ;
+        HashMap<Integer,Integer> map = new HashMap<>();
+        map.put(0, 1);
+
+        int count =0;
+
+        for(int num : nums){
+            prefix += num;
+
+            if(map.containsKey(prefix-k)){
+                count += map.get(prefix-k);
+            }
+
+            map.put(prefix, map.getOrDefault(map.get(prefix - k), 0)+1);
+        }
+
+
+        return count;
+    }
+
+    public static void main(String[] args) {
+
+        int[] nums = {1, 1, 1};
+        int k = 2;
+
+        int result = subarraySum(nums, k);
+
+        System.out.println(result);
+    }
+}
