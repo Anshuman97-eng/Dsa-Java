046254e659aec4b673da357020cbb6c78a69d9cc
Anshuman <anshuman789905@gmail.com>
2026-08-17 18:34:27 +0530

    reverseList problem

---
+++ LinkedList/reverseList/Dsa-01/reverseList.java
@@ -0,0 +1,48 @@
+class Main {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node reverseList(Node head) {
+
+        Node prev = null;
+        Node curr = head;
+
+        // Your logic here
+
+        while(curr != null){
+            Node next = curr.next;
+            curr.next = prev;
+            prev = curr;
+            curr = next;
+        }
+
+
+        return prev;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        head = reverseList(head);
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
