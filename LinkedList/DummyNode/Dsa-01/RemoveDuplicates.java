0f9099509612e47fca60507d82780c5ec9cdc84a
Anshuman <anshuman789905@gmail.com>
2026-08-19 20:00:14 +0530

    RemoveDuplicates problem

---
+++ LinkedList/DummyNode/Dsa-01/RemoveDuplicates.java
@@ -0,0 +1,48 @@
+
+
+class RemoveDuplicates {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node removeDuplicates(Node head) {
+
+        Node current = head;
+
+        while (current != null && current.next != null) {
+
+            if (current.data == current.next.data) {
+                current.next = current.next.next;
+            } else {
+                current = current.next;
+            }
+        }
+
+        return head;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+        head.next = new Node(1);
+        head.next.next = new Node(2);
+        head.next.next.next = new Node(3);
+        head.next.next.next.next = new Node(3);
+
+        head = removeDuplicates(head);
+
+        Node temp = head;
+
+        while (temp != null) {
+            System.out.print(temp.data + " ");
+            temp = temp.next;
+        }
+    }
+}
