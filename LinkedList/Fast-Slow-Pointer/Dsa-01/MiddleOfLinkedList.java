36c675b59ae9c74224eb1215f960fff4925544b2
Anshuman <anshuman789905@gmail.com>
2026-08-19 11:04:58 +0530

    MiddleOfLinkedList problem

---
+++ LinkedList/Fast-Slow-Pointer/Dsa-01/MiddleOfLinkedList.java
@@ -0,0 +1,42 @@
+public class MiddleOfLinkedList {
+
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static Node findMiddle(Node head) {
+
+        Node slow = head;
+        Node fast = head;
+
+        // Write your logic here
+
+        while(fast != null && fast.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+        }
+
+
+        return slow;
+    }
+
+    public static void main(String[] args) {
+
+        Node head = new Node(1);
+
+        head.next = new Node(2);
+        head.next.next = new Node(3);
+        head.next.next.next = new Node(4);
+        head.next.next.next.next = new Node(5);
+
+        Node middle = findMiddle(head);
+
+        System.out.println("Middle: " + middle.data);
+    }
+}
