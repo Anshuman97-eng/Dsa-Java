d89f76d61eef04fb378c47d47de3c29c10e3b711
Anshuman <anshuman789905@gmail.com>
2026-08-23 13:46:31 +0530

    LinkedListCycle problem

---
+++ LinkedList/Cycle/Dsa-01/LinkedListCycle.java
@@ -0,0 +1,46 @@
+
+
+public class LinkedListCycle {
+
+    public static void main(String[] args) {
+
+        ListNode head = new ListNode(1);
+        head.next = new ListNode(2);
+        head.next.next = new ListNode(3);
+        head.next.next.next = new ListNode(4);
+
+        // Create cycle
+        head.next.next.next.next = head.next;
+
+        System.out.println(hasCycle(head));
+    }
+
+    public static boolean hasCycle(ListNode head) {
+
+        // Write your code here
+        ListNode slow = head;
+        ListNode fast = head;
+
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+            if (slow == fast) {
+                return true;
+            }
+        }
+
+        return false;
+
+    }
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+        this.next = null;
+    }
+}
