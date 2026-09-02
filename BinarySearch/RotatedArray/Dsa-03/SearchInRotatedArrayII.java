0755f63408dcea549ba581eb300ea8074efad03b
Anshuman <anshuman789905@gmail.com>
2026-09-02 19:30:10 +0530

    SearchInRotatedArrayII problem

--- BinarySearch/RotatedArray/Dsa-03/SearchInRotatedArrayII.java
+++ BinarySearch/RotatedArray/Dsa-03/SearchInRotatedArrayII.java
@@ -6,30 +6,35 @@ public class SearchInRotatedArrayII {
         while (low <= high) {
             int mid = low + (high - low) / 2;
 
-            if(nums[mid] == target){
+            if (nums[mid] == target) {
                 return true;
-            }else if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+            }
+
+            if (nums[low] == nums[mid] && nums[mid] == nums[high]) {
                 low++;
                 high--;
-            }else if(nums[low] <= nums[mid]){
-                //left side sorted ..
-                if(target >= nums[low] && target < nums[mid]){
-                    high = mid - 1;
-                }else{
-                    low = mid + 1;
+            }
+            else {
+                if (nums[low] <= nums[mid]) {
+                    // left side sorted
+                    if (target >= nums[low] && target < nums[mid]) {
+                        high = mid - 1;
+                    } else {
+                        low = mid + 1;
+                    }
                 }
-            }else{
-                // right side sorted ..
-                if(target > nums[mid] && target <= nums[high]){
-                    low = mid + 1;
-                }else{
-                    high =mid - 1;
+                else {
+                    // right side sorted
+                    if (target > nums[mid] && target <= nums[high]) {
+                        low = mid + 1;
+                    } else {
+                        high = mid - 1;
+                    }
                 }
-
             }
         }
 
-        return false; // 👉 return true if found, else false
+        return false; // not found
     }
 
     public static void main(String[] args) {
