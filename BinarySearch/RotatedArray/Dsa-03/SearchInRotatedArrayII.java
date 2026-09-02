ccc335a6626a87eb31da7a00cd31f32ab61f72f9
Anshuman <anshuman789905@gmail.com>
2026-09-02 19:24:48 +0530

    SearchInRotatedArrayII problem

---
+++ BinarySearch/RotatedArray/Dsa-03/SearchInRotatedArrayII.java
@@ -0,0 +1,41 @@
+public class SearchInRotatedArrayII {
+    public static boolean search(int[] nums, int target) {
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+            int mid = low + (high - low) / 2;
+
+            if(nums[mid] == target){
+                return true;
+            }else if(nums[low] == nums[mid] && nums[mid] == nums[high]){
+                low++;
+                high--;
+            }else if(nums[low] <= nums[mid]){
+                //left side sorted ..
+                if(target >= nums[low] && target < nums[mid]){
+                    high = mid - 1;
+                }else{
+                    low = mid + 1;
+                }
+            }else{
+                // right side sorted ..
+                if(target > nums[mid] && target <= nums[high]){
+                    low = mid + 1;
+                }else{
+                    high =mid - 1;
+                }
+
+            }
+        }
+
+        return false; // 👉 return true if found, else false
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {2, 5, 6, 0, 0, 1, 2};
+        int target = 0;
+        boolean result = search(nums, target);
+        System.out.println("Target found? " + result);
+    }
+}
