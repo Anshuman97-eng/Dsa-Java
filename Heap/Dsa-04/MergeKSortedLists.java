178149dbf53293d4e808be6ce7b32733209051d2
Anshuman <anshuman789905@gmail.com>
2026-09-24 17:36:56 +0530

    MergeKSortedLists problem

---
+++ Heap/Dsa-04/MergeKSortedLists.java
@@ -0,0 +1,74 @@
+
+import java.util.PriorityQueue;
+
+
+
+public class MergeKSortedLists {
+
+    public static ListNode mergeKLists(ListNode[] lists) {
+        PriorityQueue<ListNode> pq = new PriorityQueue<>((a,b) -> a.val - b.val);
+        for(ListNode list : lists){
+            if(list != null){
+                pq.offer(list);
+            }
+        }
+
+        ListNode dummy = new ListNode(-1);
+        ListNode tail = dummy;
+
+        while(!pq.isEmpty()){
+            ListNode a = pq.peek();
+            tail.next = a;
+            pq.poll();
+
+            if(a.next != null){
+                pq.add(a.next);
+            }
+
+            tail = tail.next;
+        }
+        return dummy.next;
+    }
+
+    public static void printList(ListNode head) {
+
+        while (head != null) {
+
+            System.out.print(head.val + " ");
+
+            head = head.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+
+        ListNode l1 = new ListNode(1);
+        l1.next = new ListNode(4);
+        l1.next.next = new ListNode(5);
+
+        ListNode l2 = new ListNode(1);
+        l2.next = new ListNode(3);
+        l2.next.next = new ListNode(4);
+
+        ListNode l3 = new ListNode(2);
+        l3.next = new ListNode(6);
+
+        ListNode[] lists = { l1, l2, l3 };
+
+        ListNode result = mergeKLists(lists);
+
+        printList(result);
+    }
+}
+
+class ListNode {
+
+    int val;
+    ListNode next;
+
+    ListNode(int val) {
+        this.val = val;
+    }
+}
